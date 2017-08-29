4a6c2bd4c9acafdd222e979aa385a32c22cf0455
udeved <artoo@cromnix.org>
2017-08-29 18:12:57 +0200

    buildpkg, checkpkg, deploypkg: fixes

--- lib/util-pkg-publish.sh
+++ lib/util-pkg-publish.sh
@@ -12,25 +12,38 @@
 import ${LIBDIR}/util-pkg.sh
 
 del_from_repo(){
-    local repo="$1" arch="$2" pkg="$3"
-    local repo_db=${repos_root}/$repo/os/$arch/$repo.db.tar.xz
-    repo-remove -R $repo_db "${repos_root}/$repo/os/$arch/$pkg"
+    local repo="$1" destarch="$2" pkg="$3" ver pkgfile ext=pkg.tar.xz result
+    cd $pkg
+        source PKGBUILD
+        local repo_db=${repos_root}/$repo/os/$destarch/$repo.db.tar.xz
+        for name in ${pkgname[@]};do
+            [[ $arch == any ]] && CARCH=any
+            ver=$(get_full_version $name)
+            if ! result=$(find_cached_package "$name" "$ver" "$CARCH");then
+                pkgfile=$name-$ver-$CARCH.$ext
+                repo-remove -R $repo_db $name
+                rm ${repos_root}/$repo/os/$destarch/$pkgfile{,.sig}
+            fi
+        done
+    cd ..
 }
 
 add_to_repo(){
-    local repo="$1" destarch="$2" pkg="$3" ext=pkg.tar.xz ver pkgfile
-    source $pkg/PKGBUILD
-    local repo_db=${repos_root}/$repo/os/$destarch/$repo.db.tar.xz dest=$pkg
-    for name in ${pkgname[@]};do
-        [[ $arch == any ]] && CARCH=any
-        ver=$(get_full_version $name)
-        if ! $(find_cached_package "$name" "$ver" "$CARCH"); then
-            pkgfile=$name-$ver-$CARCH.$ext
-            [[ -n ${PKGDEST} ]] && dest=${PKGDEST}/$pkgfile
-            ln -sf $dest{,.sig} ${repos_root}/$repo/os/$destarch/
-            repo-add -R $repo_db ${repos_root}/$repo/os/$destarch/$pkgfile
-        fi
-    done
+    local repo="$1" destarch="$2" pkg="$3" ext=pkg.tar.xz ver pkgfile result
+    cd $pkg
+        source PKGBUILD
+        local repo_db=${repos_root}/$repo/os/$destarch/$repo.db.tar.xz dest=$pkg
+        for name in ${pkgname[@]};do
+            [[ $arch == any ]] && CARCH=any
+            ver=$(get_full_version $name)
+            if ! result=$(find_cached_package "$name" "$ver" "$CARCH"); then
+                pkgfile=$name-$ver-$CARCH.$ext
+                [[ -n ${PKGDEST} ]] && dest=${PKGDEST}/$pkgfile
+                repo-add -R $repo_db $dest
+                ln -sf $dest{,.sig} ${repos_root}/$repo/os/$destarch/
+            fi
+        done
+    cd ..
 }
 
 # upload(){
