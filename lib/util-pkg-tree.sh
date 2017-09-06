bcdeb2f3372cbfae791457d71e54c3319f2e9784
udeved <artoo@cromnix.org>
2017-09-06 17:16:22 +0200

    buildtree: import from testing if exists

--- lib/util-pkg-tree.sh
+++ lib/util-pkg-tree.sh
@@ -64,14 +64,9 @@ sync_tree_arch(){
 }
 
 read_import_list(){
-    local name="$1"
+    local repo="$1"
     local _space="s| ||g" _clean=':a;N;$!ba;s/\n/ /g' _com_rm="s|#.*||g"
-    import_list=$(sed "$_com_rm" "${list_dir_import}/$name.list" | sed "$_space" | sed "$_clean")
-}
-
-get_pkgver(){
-    source PKGBUILD
-    echo $pkgver-$pkgrel
+    import_list=$(sed "$_com_rm" "${list_dir_import}/$repo.list" | sed "$_space" | sed "$_clean")
 }
 
 is_untracked(){
@@ -88,15 +83,38 @@ import_from_arch(){
             git checkout $branch &> /dev/null
             $(is_dirty) && die "[%s] has uncommited changes!" "${repo}"
             git pull origin "$branch" #&> /dev/null
-            local arch_dir=packages
-            [[ $repo == "galaxy" ]] && arch_dir=community
+            local arch_dir=packages arch_repo src
+            case $repo in
+                system)
+                    arch_repo=core
+                    if [[ -d ${tree_dir_arch}/$arch_dir/$pkg/repos/$arch_repo-x86_64 ]] \
+                    && [[ -d ${tree_dir_arch}/$arch_dir/$pkg/repos/$testing-x86_64 ]];then
+                        src=${tree_dir_arch}/$arch_dir/$pkg/repos/$testing-x86_64
+                    fi
+                ;;
+                world)
+                    arch_repo=extra
+                    if [[ -d ${tree_dir_arch}/$arch_dir/$pkg/repos/$arch_repo-x86_64 ]] \
+                    && [[ -d ${tree_dir_arch}/$arch_dir/$pkg/repos/$testing-x86_64 ]];then
+                        src=${tree_dir_arch}/$arch_dir/$pkg/repos/$testing-x86_64
+                    fi
+                ;;
+                galaxy)
+                    arch_repo=community
+                    arch_dir=$arch_repo
+                    if [[ -d ${tree_dir_arch}/$arch_dir/$pkg/repos/$arch_repo-x86_64 ]] \
+                    && [[ -d ${tree_dir_arch}/$arch_dir/$pkg/repos/$arch_repo-$testing-x86_64 ]];then
+                        src=${tree_dir_arch}/$arch_dir/$pkg/repos/$arch_repo-$testing-x86_64
+                    fi
+                ;;
+            esac
             msg "Import into [%s]" "$repo"
             for pkg in ${import_list[@]};do
-                rsync "${rsync_args[@]}" ${tree_dir_arch}/$arch_dir/$pkg/trunk/ ${tree_dir_artix}/$repo/$pkg/
+                rsync "${rsync_args[@]}" $src/ ${tree_dir_artix}/$repo/$pkg/
                 if $(is_dirty) || $(is_untracked); then
                     git add $pkg
                     cd $pkg
-                        local ver=$(get_pkgver)
+                        local ver=$(get_full_version $pkg)
                         msg2 "Archlinux import: [%s]" "$pkg-$ver"
                         git commit -m "Archlinux import: $pkg-$ver"
                         sleep 10
