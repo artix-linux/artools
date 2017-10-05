e099003b31c55732a829b0877446e0d1c396834e
udeved <artoo@cromnix.org>
2017-10-05 12:54:35 +0200

    util-iso: drop util-iso-mount

--- lib/util-iso.sh
+++ lib/util-iso.sh
@@ -12,8 +12,62 @@
 import ${LIBDIR}/util-chroot.sh
 import ${LIBDIR}/util-iso-grub.sh
 import ${LIBDIR}/util-iso-yaml.sh
-import ${LIBDIR}/util-iso-mount.sh
 import ${LIBDIR}/util-iso-profile.sh
+
+track_img() {
+    info "mount: [%s]" "$2"
+    mount "$@" && IMG_ACTIVE_MOUNTS=("$2" "${IMG_ACTIVE_MOUNTS[@]}")
+}
+
+mount_img() {
+    IMG_ACTIVE_MOUNTS=()
+    mkdir -p "$2"
+    track_img "$1" "$2"
+}
+
+umount_img() {
+    if [[ -n ${IMG_ACTIVE_MOUNTS[@]} ]];then
+        info "umount: [%s]" "${IMG_ACTIVE_MOUNTS[@]}"
+        umount "${IMG_ACTIVE_MOUNTS[@]}"
+        unset IMG_ACTIVE_MOUNTS
+        rm -r "$1"
+    fi
+}
+
+track_fs() {
+    info "overlayfs mount: [%s]" "$5"
+    mount "$@" && FS_ACTIVE_MOUNTS=("$5" "${FS_ACTIVE_MOUNTS[@]}")
+}
+
+mount_overlay(){
+    FS_ACTIVE_MOUNTS=()
+    local lower= upper="$1" work="$2" pkglist="$3"
+    local fs=${upper##*/}
+    local rootfs="$work/rootfs" desktopfs="$work/desktopfs" livefs="$work/livefs"
+    mkdir -p "${mnt_dir}/work"
+    mkdir -p "$upper"
+    case $fs in
+        desktopfs) lower="$rootfs" ;;
+        livefs)
+            lower="$rootfs"
+            [[ -f $pkglist ]] && lower="$desktopfs":"$rootfs"
+        ;;
+        bootfs)
+            lower="$livefs":"$rootfs"
+            [[ -f $pkglist ]] && lower="$livefs":"$desktopfs":"$rootfs"
+        ;;
+    esac
+    track_fs -t overlay overlay -olowerdir="$lower",upperdir="$upper",workdir="${mnt_dir}/work" "$upper"
+}
+
+umount_overlay(){
+    if [[ -n ${FS_ACTIVE_MOUNTS[@]} ]];then
+        info "overlayfs umount: [%s]" "${FS_ACTIVE_MOUNTS[@]}"
+        umount "${FS_ACTIVE_MOUNTS[@]}"
+        unset FS_ACTIVE_MOUNTS
+        rm -rf "${mnt_dir}/work"
+    fi
+}
 
 error_function() {
     if [[ -p $logpipe ]]; then
