1ded762a78e28b6af951baab7f1595d60a11c29b
udeved <artoo@cromnix.org>
2017-11-03 18:41:21 +0100

    use makepkg library instead of local function copies

--- lib/util-msg.sh
+++ lib/util-msg.sh
@@ -8,59 +8,20 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-export LC_MESSAGES=C
+# export LC_MESSAGES=C
 export LANG=C
 
-declare ALL_OFF='' BOLD='' BLUE='' GREEN='' RED='' YELLOW=''
+source /usr/share/makepkg/util.sh
 
 if [[ -t 2 ]]; then
-    # prefer terminal safe colored and bold text when tput is supported
-    if tput setaf 0 &>/dev/null; then
-        ALL_OFF="$(tput sgr0)"
-        BOLD="$(tput bold)"
-        RED="${BOLD}$(tput setaf 1)"
-        GREEN="${BOLD}$(tput setaf 2)"
-        YELLOW="${BOLD}$(tput setaf 3)"
-        BLUE="${BOLD}$(tput setaf 4)"
-    else
-        ALL_OFF="\e[0m"
-        BOLD="\e[1m"
-        RED="${BOLD}\e[31m"
-        GREEN="${BOLD}\e[32m"
-        YELLOW="${BOLD}\e[33m"
-        BLUE="${BOLD}\e[34m"
-    fi
+    colorize
+else
+    declare -gr ALL_OFF='' BOLD='' BLUE='' GREEN='' RED='' YELLOW=''
 fi
-readonly ALL_OFF BOLD BLUE GREEN RED YELLOW
-
-plain() {
-    local mesg=$1; shift
-    printf "${BOLD}    ${mesg}${ALL_OFF}\n" "$@" >&2
-}
-
-msg() {
-    local mesg=$1; shift
-    printf "${GREEN}==>${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
-}
-
-msg2() {
-    local mesg=$1; shift
-    printf "${BLUE}  ->${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
-}
 
 info() {
     local mesg=$1; shift
     printf "${YELLOW} -->${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
-}
-
-warning() {
-    local mesg=$1; shift
-    printf "${YELLOW}==> WARNING:${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
-}
-
-error() {
-    local mesg=$1; shift
-    printf "${RED}==> ERROR:${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
 }
 
 stat_busy() {
