92ec5eb194c59e813cf037364b9ec71c0e9d2b43
udeved <artoo@cromnix.org>
2018-05-28 10:10:12 +0200

    util-iso-yaml: adopt runitcfg changes

--- lib/util-iso-yaml.sh
+++ lib/util-iso-yaml.sh
@@ -30,12 +30,18 @@ write_users_conf(){
 
 write_servicescfg_conf(){
     local init="$2"
-    local conf="$1"/"$init"cfg.conf
+    local conf="$1"/"$init"cfg.conf state='add'
     msg2 "Writing %s ..." "${conf##*/}"
     echo '---' >  "$conf"
+    if [[ "$init" == 'runit' ]];then
+        state='enabled'
+        echo 'svdir: /etc/runit/sv' >> "$conf"
+        echo '' >> "$conf"
+        echo 'runsvdir: /etc/runit/runsvdir' >> "$conf"
+    fi
     echo '' >> "$conf"
     echo 'services:' >> "$conf"
-    echo '    enabled:' >> "$conf"
+    echo "    $state:" >> "$conf"
     for svc in ${services[@]};do
         echo "      - name: $svc" >> "$conf"
         echo '        runlevel: default' >> "$conf"
